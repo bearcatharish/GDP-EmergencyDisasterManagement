762c80079283ea51686506b9dce7f2a53b92abb6
KairamkondaKalyanDass <S525907@mail.nwmissouri.edu>
2017-02-19 21:46:20 -0600

    server connection

--- public_html/server.js
+++ public_html/server.js
@@ -17,7 +17,19 @@ var router = express.Router();
 var mongo = require('mongodb');
 var mongoClient = mongo.MongoClient;
 var autoIncrement = require("mongodb-autoincrement");
-
+/*
+ app.post('/someFunction', function (req, res) {
+ Parse.Cloud.run('someFunction', req.body).then(
+ function () {
+ console.log('Cloud Code Function called');
+ return res.send(200);
+ },
+ function (error) {
+ console.log("CLoud Code Function Called Fail");
+ return res.status(400).send(error.message);
+ });
+});
+ */
 //View Engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
